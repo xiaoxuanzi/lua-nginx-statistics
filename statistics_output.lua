0fd0b4fc894d14b01de3d6064eaa3bf573799333
xiaoxuanzi <xiaoximou@gmail.com>
2020-01-22 10:44:13 +0800

    Update statistics_output.lua

--- statistics_output.lua
+++ statistics_output.lua
@@ -7,23 +7,23 @@ local function output()
 
     for _, k in pairs(keys) do
 
-		local value = statistics_dict:get(k)
-		local s, e = string.find(k, '|')
-		local upst_name = string.sub(k, 1, s -1)
-		local metric = string.sub(k,  e + 1)
+	local value = statistics_dict:get(k)
+	local s, e = string.find(k, '|')
+	local upst_name = string.sub(k, 1, s -1)
+	local metric = string.sub(k,  e + 1)
 
-		if res[ upst_name ] == nil then
-			res[ upst_name ] = {}
-		end
+	if res[ upst_name ] == nil then
+		res[ upst_name ] = {}
+	end
 
-		res[upst_name][metric] = value
+	res[upst_name][metric] = value
 
-		if string.find(metric, "err_count") then
-			if res[upst_name]["err_count"] == nil then
-				res[upst_name]["err_count"] = 0
-			end
-			res[upst_name]["err_count"] = res[upst_name]["err_count"] + value
-		end
+	if string.find(metric, "err_count") then
+	    if res[upst_name]["err_count"] == nil then
+		res[upst_name]["err_count"] = 0
+	    end
+	    res[upst_name]["err_count"] = res[upst_name]["err_count"] + value
+	end
 
     end
 
